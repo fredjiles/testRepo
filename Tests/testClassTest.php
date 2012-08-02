7554fa68e01fbf1684d580427d16ac0e26d29cf1
Fred Jiles <fredjiles@gmail.com>
2012-08-02 13:45:35 -0400

    adding in unit test to test with

---
+++ Tests/testClassTest.php
@@ -0,0 +1,10 @@
+<?php
+include_once('testClass.php');
+
+class testClassTest extends \PHPUnit_Framework_TestCase
+{
+    public function testCanTest()
+    {
+        $this->assertTrue(true);
+    }
+}
